0edcd700bd8a707965c47d6c5a4f83f486af59ed
Thomas Beaugendre <thomas-frederic.beaugendre@polymtl.ca>
2024-02-03 13:08:19 -0500

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"plotpal-25e9e","appId":"1:379680086741:web:84cad972e1307d10bfe05c","storageBucket":"plotpal-25e9e.appspot.com","apiKey":"AIzaSyAVM6972t4uPDQ5YP1XvqhrFWs0EVU1SUQ","authDomain":"plotpal-25e9e.firebaseapp.com","messagingSenderId":"379680086741","measurementId":"G-3XFJ8LDW9C"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
