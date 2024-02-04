7cdb50b63b851d4bb713ad78fa2defd14dae65f8
Bouh <bouh.abdillahi@polymtl.ca>
2024-02-04 00:52:06 -0500

    added plot page router link and plot page button in hamburger menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Auth, user } from '@angular/fire/auth';
 import { AsyncPipe } from '@angular/common'
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
