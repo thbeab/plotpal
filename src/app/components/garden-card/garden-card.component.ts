1ed117685f66b52d45d8fe432e61387e41e420be
Thomas Beaugendre <thomas-frederic.beaugendre@polymtl.ca>
2024-02-03 19:10:23 -0500

    route to garden

--- src/app/components/garden-card/garden-card.component.ts
+++ src/app/components/garden-card/garden-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Garden } from '../../interfaces/garden';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-garden-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './garden-card.component.html',
   styleUrl: './garden-card.component.css'
 })
